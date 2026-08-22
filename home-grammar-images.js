f7aac9b2223851ce8d687c50c360520ea3a70a57
sasilva-cyber <sasilva@id.uff.br>
2026-08-22 20:43:32 -0300

    Atualiza cache do menu principal

--- home-grammar-images.js
+++ home-grammar-images.js
@@ -145,7 +145,7 @@
     if (document.getElementById("footer-menu-simple-loader")) return;
     const menu = document.createElement("script");
     menu.id = "footer-menu-simple-loader";
-    menu.src = "footer-menu-simple.js?v=20260822-2041";
+    menu.src = "footer-menu-simple.js?v=20260822-2042";
     document.head.appendChild(menu);
   };
   document.head.appendChild(script);
