e0e859f1e0b6e87d0e1302ea74578e70ccf31440
sasilva-cyber <sasilva@id.uff.br>
2026-08-22 21:15:32 -0300

    Atualiza carregamento dos estilos da home

--- vocabulary-pagination.js
+++ vocabulary-pagination.js
@@ -186,6 +186,6 @@
   if (document.getElementById("home-grammar-images-loader")) return;
   const script = document.createElement("script");
   script.id = "home-grammar-images-loader";
-  script.src = "home-grammar-images.js?v=20260822-2053";
+  script.src = "home-grammar-images.js?v=20260822-2112";
   document.head.appendChild(script);
 })();
