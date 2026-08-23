177199488124e800da0a96bf41467aac4e61005c
sasilva-cyber <sasilva@id.uff.br>
2026-08-22 21:26:10 -0300

    Atualiza cache da tipografia da home

--- footer-menu-simple.js
+++ footer-menu-simple.js
@@ -51,7 +51,7 @@
     const link = document.createElement("link");
     link.id = "home-typography-styles";
     link.rel = "stylesheet";
-    link.href = "home-typography.css?v=20260822-2112";
+    link.href = "home-typography.css?v=20260822-2122";
     document.head.appendChild(link);
   }
 
